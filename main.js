ecbe038146261ccea53aeba1b72ef27ebf5b5ad0
Aydin Joshi <ajoshi1336@gmail.com>
2023-12-13 07:25:37 -0500

    Our Picks Section

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-/*
+
 document.addEventListener('DOMContentLoaded', function() {
-    searchBooks(); // Display default search results on page load
     fetchOurBooks(); // Fetch and display Harry Potter books
 });
 
@@ -13,7 +12,7 @@ function fetchOurBooks() {
                 displayOurBooks(data.items); // Display all Harry Potter books
             } else {
                 const ourPicksContainer = document.getElementById('our-picks-container');
-                ourPicksContainer.innerHTML = '<p>No Harry Potter books found.</p>';
+                ourPicksContainer.innerHTML = '<p>No books found.</p>';
             }
         })
         .catch(error => console.error('Error:', error));
@@ -24,17 +23,44 @@ function displayOurBooks(books) {
     const ourPicksContainer = document.getElementById('our-picks-container');
 
     if (!books || books.length === 0) {
-        ourPicksContainer.innerHTML = '<p>No Harry Potter books found.</p>';
+        ourPicksContainer.innerHTML = '<p>No books found.</p>';
         return;
     }
 
     books.forEach(book => {
-        const volumeInfo = book.volumeInfo;
-        const bookElement = createBookElement(volumeInfo);
-        ourPicksContainer.appendChild(bookElement);
+        const bookElement = createBookElement(book);
+
+        if (bookElement) {
+            ourPicksContainer.appendChild(bookElement);
+        }
     });
 }
-*/
+
+function createBookElement(book) {
+    const volumeInfo = book.volumeInfo;
+
+    if (!volumeInfo || !volumeInfo.title) {
+        return null; // Return null for invalid book data
+    }
+
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+
+    const title = volumeInfo.title ? volumeInfo.title : 'Title not available';
+    const authors = volumeInfo.authors ? volumeInfo.authors.join(', ') : 'Author not available';
+    const thumbnail = volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : '';
+    const infoLink = volumeInfo.infoLink ? volumeInfo.infoLink : '#';
+
+    bookElement.innerHTML = `
+        <a href="${infoLink}" target="_blank">
+            <img src="${thumbnail}" alt="${title}">
+            <p class="book-title">${title}</p>
+            <p>By: ${authors}</p>
+        </a>
+    `;
+
+    return bookElement;
+}
 
 
 /* Searches for a book */
